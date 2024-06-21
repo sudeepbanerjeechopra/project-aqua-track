21ef1180f97a2d5165b55afe3e8dfea87b6705c1
Kristina <kr.hranovska@gmail.com>
2024-06-21 17:19:26 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 // import { persistor, store } from './redux/store.js';
-
 import './shared/style/index.css';
 import 'modern-normalize';
 
