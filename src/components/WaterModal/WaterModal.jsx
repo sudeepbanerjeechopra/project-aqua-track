75a95e93cd6416f659df841153544990fc13201c
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-06-22 10:27:40 +0300

    Changes WaterModal.jsx, added WaterModal.module.css

--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,6 +1,7 @@
-import WaterForm from "../../components/WaterForm/WaterForm.jsx";
+// import WaterForm from "../../components/WaterForm/WaterForm.jsx";
 import ModalWindow from "../../shared/components/ModalWindow/ModalWindow.jsx";
 import { useModal } from "../../hooks/useModal.js";
+import css from './WaterModal.module.css';
 
 export const WaterModal = ({ operationType }) => {
 
@@ -8,7 +9,7 @@ export const WaterModal = ({ operationType }) => {
 
     const title = operationType === 'add' ? 'Add water' : 'Edit the entered amount of water';
     const subTitle = operationType === 'add' ? 'Choose a value:' : 'Correct entered data:';
-   
+  
   return (
   <>
     <button onClick={feedbackModal.openModal}>click</button>
@@ -17,11 +18,13 @@ export const WaterModal = ({ operationType }) => {
         isOpen={feedbackModal.isOpen}
         onRequestClose={feedbackModal.closeModal}
       >
-        <div>
-          <h2>{title}</h2>
-          <p>{subTitle}</p>
-          <WaterForm operationType={operationType} />  
-        </div>
+        <div className="modal-content">
+          <div className={css.modalWater}>
+          <h2 className={css.titleWaterModal}>{title}</h2>
+          <p className={css.subTitleWaterModal}>{subTitle}</p>
+          {/* <WaterForm operationType={operationType} />   */}
+          </div>
+          </div>
     </ModalWindow> 
         
   </>
