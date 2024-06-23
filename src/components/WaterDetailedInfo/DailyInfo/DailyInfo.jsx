bc60a7c09f35aed4bb90dde9b1464e10f697d29f
sno0wi <sno0wi20@gmail.com>
2024-06-23 12:07:02 +0300

    add folders and files

---
+++ src/components/WaterDetailedInfo/DailyInfo/DailyInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DailyInfo() {
+  return (
+    <div>DailyInfo</div>
+  )
+}
+
+export default DailyInfo
---
+++ src/components/WaterDetailedInfo/DailyInfo/WaterItem/WaterItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WaterItem() {
+  return (
+    <div>WaterItem</div>
+  )
+}
+
+export default WaterItem
