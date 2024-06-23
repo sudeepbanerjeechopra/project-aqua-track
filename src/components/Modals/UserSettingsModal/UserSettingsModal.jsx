502204d2b4d245b7a683ce1f84946e8c817d7770
alex_horobij <olexa.horobij@gmail.com>
2024-06-24 00:29:27 +0300

    some stuff

--- src/components/Modals/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/Modals/UserSettingsModal/UserSettingsModal.jsx
@@ -3,6 +3,7 @@ import css from './UserSettingsModal.module.css';
 import ModalWindow from '../../../shared/components/ModalWindow/ModalWindow';
 import { useEffect, useRef, useState } from 'react';
 import { icons as sprite } from '../../../shared/icons/index';
+import CustomScrollWrapper from '../../../shared/components/CustomScrollWrapper/CustomScrollWrapper';
 
 const UserSettingsModal = ({
   isOpen,
@@ -46,39 +47,82 @@ const UserSettingsModal = ({
       onRequestClose={closeModal}
       shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
     >
-      <div className={css.wrapper}>
-        <form onSubmit={undefined} className={css.form}>
-          <div className={css.userPic}>
-            <h2>Setting</h2>
-            <div className={css.picWrapper}>
-              <div className={css.pic}>
-                <img
-                  src={
-                    avatarFile
-                      ? URL.createObjectURL(avatarFile)
-                      : 'https://st.depositphotos.com/1537427/3571/v/450/depositphotos_35717211-stock-illustration-vector-user-icon.jpg'
-                  }
-                  alt="avatar"
+      <CustomScrollWrapper>
+        <div className={css.wrapper}>
+          <form onSubmit={undefined} className={css.form}>
+            <div className={css.userPic}>
+              <h2>Setting</h2>
+              <div className={css.picWrapper}>
+                <div className={css.pic}>
+                  <img
+                    src={
+                      avatarFile
+                        ? URL.createObjectURL(avatarFile)
+                        : 'user-icon.jpg'
+                    }
+                    alt="avatar"
+                  />
+                </div>
+                <div className={css.uploadWrapper} onClick={handleClick}>
+                  <svg className={css.iconUpload}>
+                    <use xlinkHref={`${sprite}#upload`} />
+                  </svg>
+                  <div className={css.textUpload}>Upload a photo</div>
+                </div>
+                <input
+                  type="file"
+                  onChange={handleChange}
+                  ref={hiddenFileInput}
+                  style={{ display: 'none' }}
                 />
               </div>
-              <div className={css.uploadWrapper} onClick={handleClick}>
-                <svg className={css.iconUpload}>
-                  <use xlinkHref={`${sprite}#upload`} />
-                </svg>
-                <div className={css.textUpload}>Upload a photo</div>
-              </div>
-              <input
-                type="file"
-                onChange={handleChange}
-                ref={hiddenFileInput}
-                style={{ display: 'none' }}
-              />
             </div>
-          </div>
-          <div className={css.inputs}></div>
-          <div className={css.buttonContainer}></div>
-        </form>
-      </div>
+            <div className={css.inputs}>
+              <div className={css.midContainer}>
+                <h3>Your gender identity</h3>
+                <div className={css.radioContainer}>
+                  <div className={css.radioButton}>
+                    <input
+                      className={css.radio}
+                      type="radio"
+                      name="gender"
+                      id="woman"
+                    />
+                    <label className={css.radioLabel} htmlFor="woman">
+                      Woman
+                    </label>
+                  </div>
+                  <div className={css.radioButton}>
+                    <input
+                      className={css.radio}
+                      type="radio"
+                      name="gender"
+                      id="man"
+                    />
+                    <label className={css.radioLabel} htmlFor="man">
+                      Man
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className={css.midContainer}>
+                <div>
+                  <h3>Your name</h3>
+                  <input type="text" name="" id="" />
+                </div>
+                <div>
+                  <h3>Email</h3>
+                  <input type="email" name="" id="" />
+                </div>
+              </div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={css.buttonContainer}></div>
+          </form>
+        </div>
+      </CustomScrollWrapper>
     </ModalWindow>
   );
 };
