f5ce66ab030f320f159b2abea0f1943104594446
peychma <mariapei4eva@gmail.com>
2024-06-24 23:11:34 +0300

    1st commit

--- src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,40 +1,41 @@
 import React, { useEffect } from 'react';
 import { useModalContext } from '../../../context/useModalContext';
-import css from './DeleteWaterModal/DeleteWaterModal'
-import { icons as sprite } from '../../../shared/icons/index';
+import css from './DeleteWaterModal.module.css';
+//import { icons as sprite } from '../../../shared/icons/index';
 import { useDispatch } from 'react-redux';
 import { updateWaterProgressBar, updateWaterList, updateCalendar } from '../../redux/actions';
 
+/* <svg onClick={closeModal} className={`${style.iconClose}`}>
+          <use xlinkHref={`${sprite}#close`} />
+</svg>*/
 
 const DeleteWaterModal = () => {
-    const { closeModal } = useModalContext();
-
-      const dispatch = useDispatch();
-      const handleDelete = () => {
-    // Приклад: axios.delete(`/api/water/${modalContent.id}`)
-    // .then(response => {
-    //   if (response.status === 200) {
-    //     dispatch(updateWaterProgressBar());
-    //     dispatch(updateWaterList());
-    //     dispatch(updateCalendar());
-    //     closeModal();
-    //   } else {
-    //     // Обробка помилки і показ повідомлення
+  const { closeModal } = useModalContext();
+  const dispatch = useDispatch();
+      
+  const handleDelete = () => {
+    // axios.delete(`/api/water/${modalContent.id}`)
+    //   .then(response => {
+    //     if (response.status === 200) {
+    //       dispatch(updateWaterProgressBar());
+    //       dispatch(updateWaterList());
+    //       dispatch(updateCalendar());
+    //       closeModal();
+    //     } else {
+    //       alert('Не вдалося видалити запис.');
+    //     }
+    //   })
+    //   .catch(error => {
     //     alert('Не вдалося видалити запис.');
-    //   }
-    // })
-    // .catch(error => {
-    //   // Обробка помилки і показ повідомлення
-    //   alert('Не вдалося видалити запис.');
-    // });
+    //   });
 
     dispatch(updateWaterProgressBar());
     dispatch(updateWaterList());
     dispatch(updateCalendar());
     closeModal();
-    };
-    
-    const handleKeyDown = (event) => {
+  };
+
+  const handleKeyDown = (event) => {
     if (event.key === 'Escape') {
       closeModal();
     }
@@ -48,19 +49,16 @@ const DeleteWaterModal = () => {
   }, []);
 
   return (
-      <div>
-          {modalContent}
-          <svg onClick={closeModal} className={`${style.iconClose}`}>
-              <use xlinkHref={`${sprite}#close`} />
-          </svg>
-          <h2>Delete entry</h2>
-          <p>Are you sure you want to delete the entry?</p>
-          <div>
-              <button onclick={handleDelete} type='button'>Delete</button>
-              <button onClick={closeModal} type='button'>Cancel</button>
-          </div>
+      <div className={css.deleteModalBackground}>
+        <h2 className={css.title}>Delete entry</h2>
+        <p className={css.paragraf}>Are you sure you want to delete the entry?</p>
+        <div className={css.buttons}>
+            <button className={css.button-1} onClick={handleDelete} type='button'>Delete</button>
+            <button className={css.button-2} onClick={closeModal} type='button'>Cancel</button>
+      </div>
     </div>
   );
 };
 
 export default DeleteWaterModal;
+
