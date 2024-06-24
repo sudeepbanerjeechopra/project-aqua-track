182adca51af05c96e43285e632eb51976fbcc507
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-06-24 20:54:58 +0200

    complete styles

--- src/components/Home/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/Home/AdvantagesSection/AdvantagesSection.jsx
@@ -1,13 +1,26 @@
-import css from '../AdvantagesSection/AdvantagesSection.module.css'
+import css from '../AdvantagesSection/AdvantagesSection.module.css';
 
 const AdvantagesSection = () => {
-  return <>
-  <div className={css.bgImg}></div>
-  <div className={css.customers}></div>
-  <div className={css.benefits}>
-    <div></div>
-  </div>
-  </>;
+  return (
+    <div className={css.main}>
+      <div className={css.customers}>
+        <div className={css.customer}></div>
+        <div className={css.customer}></div>
+        <div className={css.customer}></div>
+        <p className={css.text}>Our <span className={css.coloredText}>happy</span> customers</p>
+      </div>
+      <div className={css.benefits}>
+        <div className={css.commonCont}>
+        <div className={css.firstBen}>
+          <div className={css.point}></div>
+          <p className={css.benText}>Habit drive</p>
+        </div>
+        <p className={css.secBen}>View statistics</p>
+        </div>
+        <p className={css.thirdBen}>Personal rate setting</p>
+      </div>
+    </div>
+  );
 };
 
 export default AdvantagesSection;
