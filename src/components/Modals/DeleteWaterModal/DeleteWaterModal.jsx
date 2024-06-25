0be724e8c0388af1435aed01fed518f18f95b48d
peychma <mariapei4eva@gmail.com>
2024-06-25 16:20:21 +0300

    changes

--- src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/Modals/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react';
 import { useModalContext } from '../../../context/useModalContext';
 import css from './DeleteWaterModal.module.css';
 import { useDispatch } from 'react-redux';
-import axios from '../../../helpers/axiosConfig';
-import { updateWaterProgressBar, updateWaterList, updateCalendar } from '../../redux/actions'; // буде змінено на потрібні функції
+//import axios from '../../../helpers/axiosConfig.js';
+//import { updateWaterProgressBar, updateWaterList, updateCalendar } from '../../redux/actions'; // буде змінено на потрібні функції
 
 const DeleteWaterModal = () => {
   const { closeModal } = useModalContext();
   const dispatch = useDispatch();
 
-  const handleDelete = () => {
-    /*axios.delete(`/api/water/${modalContent.id}`) // буде змінено на правильний шлях
+/*  const handleDelete = () => {
+    axios.delete(`/api/water/${modalContent.id}`) // буде змінено на правильний шлях
       .then(response => {
         if (response.status === 200) {
           dispatch(updateWaterProgressBar());
@@ -23,34 +22,20 @@ const DeleteWaterModal = () => {
       })
       .catch(error => {
         alert('Failed to delete the entry.');
-      }); */
+      }); 
 
     dispatch(updateWaterProgressBar());
     dispatch(updateWaterList());
     dispatch(updateCalendar());
     closeModal();
-  };
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        closeModal();
-      }
-    };
-
-    document.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [closeModal]);
+  }; */
 
   return (
     <div className={css.deleteModalBackground}>
       <h2 className={css.title}>Delete entry</h2>
       <p className={css.paragraf}>Are you sure you want to delete the entry?</p>
       <div className={css.buttons}>
-        <button className={css.button-1} onClick={handleDelete} type='button'>Delete</button>
+        <button className={css.button-1} type='button'>Delete</button>
         <button className={css.button-2} onClick={closeModal} type='button'>Cancel</button>
       </div>
     </div>
