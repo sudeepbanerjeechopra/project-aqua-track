9f4216a4a208f3610077123260f9b0c065f3028b
Kristina <kr.hranovska@gmail.com>
2024-06-25 16:50:40 +0300

    add loader

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -5,10 +5,10 @@ import { Helmet } from 'react-helmet-async';
 const SignInPage = () => {
   return (
     <>
-      <h2>SignInPage</h2>
       <Helmet>
         <title>Sign In</title>
       </Helmet>
+
       {/* <Container>
         <SignInForm />
         <AdvantagesSection />
--- src/shared/components/SharedLayout/SharedLayout .jsx
+++ src/shared/components/SharedLayout/SharedLayout .jsx
@@ -1,11 +1,12 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import Loader from '../../../components/Loader/Loader';
 
 const SharedLayout = () => {
   return (
     <>
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
       <Toaster position="top-right" reverseOrder={true} />
