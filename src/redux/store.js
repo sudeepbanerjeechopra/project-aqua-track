4d706785280b9e6e09a85bc19f5513e02c86cd56
sno0wi <sno0wi20@gmail.com>
2024-06-27 10:38:43 +0300

    add redux modal open

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
-import waterModalSlice from '../redux/water/slice';
 import storage from 'redux-persist/lib/storage';
 import {
-    persistReducer,
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
 
 import { authReducer } from './auth/slice';
 import { setupAxiosInterceptors } from './auth/operation';
 
 const authPersistConfig = {
-    key: 'auth',
-    storage,
-    whitelist: ['token', 'refreshToken'],
+  key: 'auth',
+  storage,
+  whitelist: ['token', 'refreshToken'],
 };
 
 const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
-    reducer: {
-        auth: persistedAuthReducer,
-        water: waterModalSlice,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
+  reducer: {
+    auth: persistedAuthReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 setupAxiosInterceptors(store);
