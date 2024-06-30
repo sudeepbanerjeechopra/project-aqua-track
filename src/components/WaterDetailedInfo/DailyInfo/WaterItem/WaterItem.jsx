dcf6a893c7c1fc9c77eab9eceab3bb6a173a2897
Kristina <kr.hranovska@gmail.com>
2024-06-30 21:34:35 +0300

    Update WaterItem.jsx

--- src/components/WaterDetailedInfo/DailyInfo/WaterItem/WaterItem.jsx
+++ src/components/WaterDetailedInfo/DailyInfo/WaterItem/WaterItem.jsx
@@ -17,9 +17,13 @@ function WaterItem({ data }) {
 
   const formatTime = (isoString) => {
     const date = new Date(isoString);
-    const hours = String(date.getHours()).padStart(2, '0');
+    let hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    return `${hours}:${minutes}`;
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const strHours = String(hours).padStart(2, '0');
+    return `${strHours}:${minutes} ${ampm}`;
   };
 
   return (
